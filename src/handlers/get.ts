4e6e379ac1b8a9c689a766f6721baa2e1bf19c9d
AtoraSuunva <atora@giraffeduck.com>
2022-08-16 00:19:38 -0400

    fix(get): check for valid ETags before using in onlyIf, preventing crashes

--- src/handlers/get.ts
+++ src/handlers/get.ts
@@ -44,10 +44,14 @@ async function handleObjectRequest(
 ): Promise<Response> {
   const key = requestPath.slice(1)
   const range = parseRange(request.headers.get('range')) ?? undefined
+  // Prevent errors if the client sends invalid ETag headers
+  const onlyIf = hasValidETagHeaders(request.headers)
+    ? request.headers
+    : undefined
 
   const object = await env.FILE_BUCKET.get(key, {
     range,
-    onlyIf: request.headers,
+    onlyIf,
   })
 
   if (object) {
@@ -67,7 +71,7 @@ async function handleObjectRequest(
   // Get again for correct range
   const objectAgain = await env.FILE_BUCKET.get(key, {
     range,
-    onlyIf: request.headers,
+    onlyIf,
   })
 
   if (objectAgain) {
@@ -78,4 +82,35 @@ async function handleObjectRequest(
   }
 
   return new Response('Failed to cache object', { status: 500 })
+}
+
+const eTagRegex = /(P<weak>W\/)?"(P<value>.*)"/
+
+const eTagHeaders = ['If-Match', 'If-None-Match']
+
+/**
+ * Checks if the ETag-related headers are valid for an ETag, since otherwise
+ * R2 errors ungracefully on invalid ETag values
+ * @param headers The headers to check
+ * @returns If all the defined ETag-related headers are valid ETags
+ */
+function hasValidETagHeaders(headers: Headers): boolean {
+  for (const header of eTagHeaders) {
+    if (!isValidETagHeader(headers.get(header))) {
+      return false
+    }
+  }
+
+  return true
+}
+
+/**
+ * Check an individual header to see if it is an valid ETag
+ * @param value The value of the header to check
+ * @returns If the value is valid for an ETag. Null is considered valid since it's considered as "header missing"
+ */
+function isValidETagHeader(value: string | null): boolean {
+  // null is ok, means the header doesn't exist
+  // but if it's not null and it fails the regex, it's invalid
+  return value === null || eTagRegex.test(value)
 }
